73519516c821025475658496e7c748e1309d9994
NikitaSNS <nik_1998@inbox.ru>
2016-04-24 13:32:47 +0400

    111

--- App1/App1/Resources/Resource.Designer.cs
+++ App1/App1/Resources/Resource.Designer.cs
@@ -78,11 +78,17 @@ namespace App1
 			// aapt resource value: 0x7f050003
 			public const int editText2 = 2131034115;
 			
+			// aapt resource value: 0x7f050004
+			public const int imageButton1 = 2131034116;
+			
 			// aapt resource value: 0x7f050000
 			public const int imageView1 = 2131034112;
 			
 			// aapt resource value: 0x7f050002
 			public const int imageView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int imageView3 = 2131034117;
 			
 			static Id()
 			{
